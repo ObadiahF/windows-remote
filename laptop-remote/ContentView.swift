d5ea74aa8c3fb58e754ec9d1375a7ac5f6d69204
ObadiahF <obadiahfusco@icloud.com>
2026-05-13 16:18:37 -0700

    Initial Commit

---
+++ laptop-remote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  laptop-remote
+//
+//  Created by Obadiah Fusco on 5/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ laptop-remote/laptop_remoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  laptop_remoteApp.swift
+//  laptop-remote
+//
+//  Created by Obadiah Fusco on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct laptop_remoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
